a658d24cfbb284d8825b283fbd3733dc57adca4b
Turgay Can <turgay.can@inomera.com>
2023-07-11 22:52:52 +0300

    Fix the comment otu code block.

--- example/spring-kafka/src/main/java/com/inomera/telco/commons/example/springkafka/SomeSender.java
+++ example/spring-kafka/src/main/java/com/inomera/telco/commons/example/springkafka/SomeSender.java
@@ -30,32 +30,32 @@ public class SomeSender {
     @Scheduled(fixedDelay = 1000)
     public void publishRandomText() {
 	LOG.info("Sending event");
-//	for (int i = 0; i < 4; i++) {
-//	    ThreadUtils.sleepQuietly(3000);
-//	    final int value = atomicInteger.incrementAndGet();
-//	    if (value % 3 == 0) {
-//		kafkaMessagePublisher.send("mouse-event.click", new SomethingHappenedMessage(value + "-" + TransactionKeyUtils.generateTxKey()));
-//	    } else if (value % 3 == 1) {
-//		kafkaMessagePublisher.send("mouse-event.dblclick", new SomethingHappenedBeautifullyMessage(value + "-" + TransactionKeyUtils.generateTxKey()));
-//	    } else if (value % 3 == 2) {
-//		kafkaMessagePublisher.send("mouse-event.dblclick", new SomethingHappenedConsumerMessage(value + "-" + TransactionKeyUtils.generateTxKey()));
-//	    } else {
-//		kafkaMessagePublisher.send("example.unlistened-topic", new UnListenedMessage(value + "-" + TransactionKeyUtils.generateTxKey()));
-//	    }
-//	}
+	for (int i = 0; i < 4; i++) {
+	    ThreadUtils.sleepQuietly(3000);
+	    final int value = atomicInteger.incrementAndGet();
+	    if (value % 3 == 0) {
+		kafkaMessagePublisher.send("mouse-event.click", new SomethingHappenedMessage(value + "-" + TransactionKeyUtils.generateTxKey()));
+	    } else if (value % 3 == 1) {
+		kafkaMessagePublisher.send("mouse-event.dblclick", new SomethingHappenedBeautifullyMessage(value + "-" + TransactionKeyUtils.generateTxKey()));
+	    } else if (value % 3 == 2) {
+		kafkaMessagePublisher.send("mouse-event.dblclick", new SomethingHappenedConsumerMessage(value + "-" + TransactionKeyUtils.generateTxKey()));
+	    } else {
+		kafkaMessagePublisher.send("example.unlistened-topic", new UnListenedMessage(value + "-" + TransactionKeyUtils.generateTxKey()));
+	    }
+	}
 	LOG.info("Sent event");
     }
 
     @Scheduled(fixedDelay = 1000)
     public void publishRandomBulkText() {
-//	LOG.info("Sending event");
+	LOG.info("Sending event");
 	for (int i = 0; i < 1; i++) {
 	    final int value = bulkAtomicInteger.incrementAndGet();
-//	    kafkaMessagePublisher.send("mouse-bulk-event.click", new SomethingHappenedMessage(value + "-" + TransactionKeyUtils.generateTxKey()));
-//	    kafkaMessagePublisher.send("mouse-bulk-event.dblclick", new SomethingHappenedBeautifullyMessage(value + "-" + TransactionKeyUtils.generateTxKey()));
+	    kafkaMessagePublisher.send("mouse-bulk-event.click", new SomethingHappenedMessage(value + "-" + TransactionKeyUtils.generateTxKey()));
+	    kafkaMessagePublisher.send("mouse-bulk-event.dblclick", new SomethingHappenedBeautifullyMessage(value + "-" + TransactionKeyUtils.generateTxKey()));
 	    kafkaMessagePublisher.send("mouse-bulk-event.dblclick", new SomethingHappenedConsumerMessage(value + "-" + TransactionKeyUtils.generateTxKey()));
-//	    kafkaMessagePublisher.send("bulk-example.unlistened-topic", new UnListenedMessage(value + "-" + TransactionKeyUtils.generateTxKey()));
+	    kafkaMessagePublisher.send("bulk-example.unlistened-topic", new UnListenedMessage(value + "-" + TransactionKeyUtils.generateTxKey()));
 	}
-//	LOG.info("Sent event");
+	LOG.info("Sent event");
     }
 }
